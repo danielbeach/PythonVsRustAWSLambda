c4b24d94254d130f58e53d92f7d393b92cba39e1
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2023-02-27 14:11:11 +0100

    Some optimizations

--- main.rs
+++ main.rs
@@ -1,14 +1,12 @@
 use aws_lambda_events::s3::S3Event;
-use flate2::read::GzDecoder;
+use flate2::bufread::GzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use flate2::GzBuilder;
 use lambda_runtime::{handler_fn, Error};
 use serde_json::Value;
-use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::Read;
+use std::io::{BufRead, BufReader};
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_runtime::Error> {
@@ -38,46 +36,35 @@ async fn handler(req: Value, _ctx: lambda_runtime::Context) -> Result<(), Box<Er
             .unwrap()
             .into_bytes();
 
-        let mut d: GzDecoder<&[u8]> = GzDecoder::new(&data[..]);
-        let mut csv_data: String = String::new();
-        d.read_to_string(&mut csv_data).unwrap();
+        let decoder: BufReader<GzDecoder<&[u8]>> = BufReader::new(GzDecoder::new(&data[..]));
 
-        let split: std::str::Lines<'_> = csv_data.lines();
-        let result_vector: Vec<&str> = split.collect();
+        let output = Vec::new();
+        let mut encoder: GzEncoder<Vec<u8>> = GzBuilder::new()
+            .filename("tab_converted.txt")
+            .write(output, Compression::default());
 
-        let mut tab_converted: String = String::new();
-        for line in result_vector.iter().skip(1) {
+        for line in decoder.lines().skip(1) {
+            let line = line.unwrap();
             let date: &&str = &line[0..14].trim();
             let serial_number: &&str = &line[15..35].trim();
             let model: &&str = &line[36..78].trim();
             let capacity_bytes: &&str = &line[79..97].trim();
             let failure: &&str = &line[98..108].trim();
-            let tab_line: String = format!(
-                "{}\t{}\t{}\t{}\t{}\n",
+            writeln!(
+                &mut encoder,
+                "{}\t{}\t{}\t{}\t{}",
                 date, serial_number, model, capacity_bytes, failure
-            );
-            tab_converted.push_str(&tab_line);
+            )
+            .unwrap();
         }
-        let f: File = File::create("/tmp/file.gz").expect("failed to create file");
-        let mut gz: GzEncoder<File> = GzBuilder::new()
-            .filename("tab_converted.txt")
-            .write(f, Compression::default());
-        gz.write_all(tab_converted.as_bytes())
-            .expect("failed to write bytes to file");
-        gz.finish().expect("failed to flush bytes to file");
-
-        let file: File = File::open("/tmp/file.gz").expect("problem reading file");
-        let mut reader: BufReader<File> = BufReader::new(file);
-        let mut buffer: Vec<u8> = Vec::new();
-
-        reader.read_to_end(&mut buffer).expect("error");
+        let output = encoder.finish().expect("failed to flush bytes to file");
 
         let remote_uri: &String = &key.replace("fixed_width_raw/", "tab_converted/");
         s3_client
             .put_object()
             .bucket(&bucket_name)
             .key(remote_uri)
-            .body(buffer.into())
+            .body(output.into())
             .send()
             .await
             .unwrap();
